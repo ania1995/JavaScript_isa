7a19428387bd48899364f3ab870922e1f42cf06f
Anna Stanek <aniastanekk@gmail.com>
2020-09-01 10:32:06 +0200

    WIP: issue3

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-// Change content of element of id="firstBlock";
-const newText = document.getElementById("firstBlock");
-newText.innerHTML = "New text DIV";
+const newElement = document.createElement("div");
+newElement.innerHTML = "Example content of new div";
 
-//Add new class="new-class";
-
-const newClass = document.querySelector(".secondParagraph");
-newClass.classList.add("new-class");
+const div = document.querySelector("#firstBlock");
+div.appendChild(newElement);

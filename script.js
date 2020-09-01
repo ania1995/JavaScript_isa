590ca615c5cc8097d2ba9e315c5de111ee501f24
Anna Stanek <aniastanekk@gmail.com>
2020-09-01 12:06:31 +0200

    Issue#4

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.dir();
+
+const newOneElement = document.createElement("div");
+newOneElement.id = "preFirstBlock";
+newOneElement.innerText = "Wstawiony PreFirst DIV";
+
+console.log(newOneElement);
+
+const previouslyElement = document.querySelector("#firstBlock");
+const parent = document.querySelector("body");
+
+parent.insertBefore(newOneElement, previouslyElement);

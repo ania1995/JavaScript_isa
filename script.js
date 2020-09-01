6573fba0e28e4670e7787af5298d4307ad460a0e
Anna Stanek <aniastanekk@gmail.com>
2020-09-01 12:42:25 +0200

    Change TextContent to innetText

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const div = document.querySelector("#firstBlock");
-div.textContent = "";
+div.innerHTML = "";
 
 //write all properties of element newClass
 console.dir(div);
 
 for (let i = 0; i < 3; i++) {
   const newElement = document.createElement("p");
-  newElement.textContent = `new paragraph ${i + 1}`;
+  newElement.innerText = `new paragraph ${i + 1}`;
   div.appendChild(newElement);
 }

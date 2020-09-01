7eedda092fc3f053287fd70d68e18d02dcb3e4e4
Anna Stanek <aniastanekk@gmail.com>
2020-09-01 11:41:27 +0200

    WIP: issue#4

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-const div = document.querySelector("#firstBlock");
-div.textContent = "";
-
-//write all properties of element newClass
-console.dir(div);
-
-for (let i = 0; i < 3; i++) {
-  const newElement = document.createElement("p");
-  newElement.textContent = `new paragraph ${i + 1}`;
-  div.appendChild(newElement);
-}
+console.dir();

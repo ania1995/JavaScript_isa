8e86deb63e889c5c429c104f0f096977a79fce5b
Anna Stanek <aniastanekk@gmail.com>
2020-09-01 12:45:33 +0200

    Add second method

--- script.js
+++ script.js
@@ -4,8 +4,17 @@ div.innerHTML = "";
 //write all properties of element newClass
 console.dir(div);
 
+//Method 1.
 for (let i = 0; i < 3; i++) {
   const newElement = document.createElement("p");
   newElement.innerText = `new paragraph ${i + 1}`;
   div.appendChild(newElement);
+}
+
+//Method 2.
+const newArray = [];
+for (let i = 0; i < 3; i++) {
+  newArray[i] = document.createElement("p");
+  newArray[i].innerText = `new paragraph ${i + 1}`;
+  div.appendChild(newArray[i]);
 }

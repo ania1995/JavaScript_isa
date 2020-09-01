cb8cbabb1def1cf59620dc6c965912552c463546
Anna Stanek <aniastanekk@gmail.com>
2020-09-01 10:04:03 +0200

    Issue1

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-var num1 = 5;
-var num2 = 10;
+// var num1 = 5;
+// var num2 = 10;
+// var sum = num1 + num2;
 
-console.log(num1 + num2);
+// console.log(sum);
+
+const allparagraph = document.querySelectorAll("p");
+console.log(allparagraph);
+
+const elementbyClass = document.querySelector(".firstParagraph");
+console.log(elementbyClass);
+
+const elementbyId = document.getElementById("firstBlock");
+console.log(elementbyId);

179f54bd9c2301397db8f53db9205742b75e88b2
Anna Stanek <aniastanekk@gmail.com>
2020-09-01 10:58:34 +0200

    Add function console.dir

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const newText = document.getElementById("firstBlock");
 newText.innerHTML = "New text DIV";
 
 //Add new class="new-class";
-
 const newClass = document.querySelector(".secondParagraph");
 newClass.classList.add("new-class");
+
+//write all properties of element newClass
+console.dir(newClass);
+
+console.dir(newText);
